d96fb274fbcf5f2969c8d51e3bd6a1522ebe2811
TotKindGott <konstantin.a.kolosov@gmail.com>
2015-03-06 23:53:45 -0500

    Modified calculation function

--- scripts.js
+++ scripts.js
@@ -14,40 +14,43 @@
         el.setAttribute("onfocus", "if(document.getElementById('" + selector + "').value == '0') {document.getElementById('" + selector + "').value = ''}");
         el.setAttribute("onblur", "if(document.getElementById('" + selector + "').value == '') {document.getElementById('" + selector + "').value = '0'}");
     }
+    document.getElementById("B100").focus();
 })();
 
 function calculate() {
 
-    function getValue(id) {
+    function one_by_one(id, multi) {
         // Validate and filter input - only digits are allowed, no leading zeroes, no decimal points
-        var result = document.getElementById(id).value.replace(/\D/g, '').replace(/^0+/, '');
+        var user_input = document.getElementById(id).value.replace(/\D/g, '').replace(/^0+/, '');
         // In case of empty input or any other non-valid value
-        if (isNaN(result)) result = 0;
+        if (isNaN(user_input)) user_input = 0;
         // Place validated value back in the text field
-        document.getElementById(id).value = result;
+        document.getElementById(id).value = user_input;
+        var result;
+        // If result is expected to be an integer, i.e. bills
+        if (multi % 1 === 0) {
+            result = parseInt((user_input) * (multi));
+        } 
+        // If result is expected to be a floating-point value
+        else {
+            result = parseFloat((user_input) * (multi));
+        }
+        // Show individual total as a tooltip over a text field
+        document.getElementById(id).setAttribute("title", "$" + result);
+//          console.log(id + ' ' + result);
         return result;
     }
 
-    var B100 = (parseInt(getValue('B100') * (100)));
-    document.getElementById('B100').setAttribute("title", "$" + B100);
-    var B50  = (parseInt(getValue('B50') * (50)));
-    document.getElementById('B50').setAttribute("title", "$" + B50);
-    var B20  = (parseInt(getValue('B20') * (20)));
-    document.getElementById('B20').setAttribute("title", "$" + B20);
-    var B10  = (parseInt(getValue('B10') * (10)));
-    document.getElementById('B10').setAttribute("title", "$" + B10);
-    var B5   = (parseInt(getValue('B5') * (5)));
-    document.getElementById('B5').setAttribute("title", "$" + B5);
-    var B1   = (parseInt(getValue('B1') * (1)));
-    document.getElementById('B1').setAttribute("title", "$" + B1);
-    var C25  = (parseFloat(getValue('C25') / (4)));
-    document.getElementById('C25').setAttribute("title", "$" + C25);
-    var C10  = (parseFloat(getValue('C10') / (10)));
-    document.getElementById('C10').setAttribute("title", "$" + C10);
-    var C5   = (parseFloat(getValue('C5') / (20)));
-    document.getElementById('C5').setAttribute("title", "$" + C5);
-    var C1   = (parseFloat(getValue('C1') / (100)));
-    document.getElementById('C1').setAttribute("title", "$" + C1);
+    var B100 = one_by_one("B100", 100);
+    var  B50 = one_by_one("B50", 50);
+    var  B20 = one_by_one("B20", 20);
+    var  B10 = one_by_one("B10", 10);
+    var   B5 = one_by_one("B5", 5);
+    var   B1 = one_by_one("B1", 1);
+    var  C25 = one_by_one("C25", 0.25);
+    var  C10 = one_by_one("C10", 0.1);
+    var   C5 = one_by_one("C5", 0.05);
+    var   C1 = one_by_one("C1", 0.01);
   
     var ALL  = parseInt(B100 + B50 + B20 + B10 + B5 + B1) + parseFloat(C25 + C10 + C5 + C1);
     var DEP  = parseFloat(ALL - 150);
